26baf5349017a702f7756907406c97d8050ada66
cryoq <cryoqq@gmail.com>
2025-03-28 14:59:33 -0500

    made encoded function functional

--- Programs/Program 2/Vigenere_Cipher.py
+++ Programs/Program 2/Vigenere_Cipher.py
@@ -1,23 +1,36 @@
-testKey = "mykey"
+import sys
 
-testString = "hello"
+def encode(plainText, key):
+    plainTextLowered = plainText.lower()
+    keyLowered = testKey.lower()
+    keyLowered = keyLowered.replace(" ", "")
 
+    encrypted = ""
+    count = 0
 
-keyLowered = testKey.lower()
+    for i in range(len(plainTextLowered)):
+        P = ord(plainTextLowered[i]) - 97
 
-for character in keyLowered:
-    print(character + ": " + (str(ord(character)-97)))
+        if (P >= 0 and P <= 25):
+            K = ord(keyLowered[count]) - 97
+            if testString[i].isupper():
+                encrypted += chr( ((P + K) % 26) + 97).upper()
+            else:
+                encrypted += chr( ((P + K) % 26) + 97)
+            count += 1
+            if count >= len(keyLowered):
+                count = 0
+        else:
+            encrypted += plainTextLowered[i]
+    return encrypted
 
+def decode(encodedText, key):
+    decodedText = encodedText # temp
+    return decodedText
 
-encrypted = ""
-count = 0
+testKey = "This is my key"
+testString = "Get ready for Cyber Storm! We're going to turn your world upside down on May 16!"
 
-for i in range(len(testString)):
-    if count >= len(testKey):
-        count = 0
-    encrypted += chr( ( ( ((ord(keyLowered[count]))-97) + ((ord(testString[i]))-97) ) % 26) + 97)
-    count += 1
+encrypted = encode(testString, testKey)
 
 print(encrypted)
-
- 
