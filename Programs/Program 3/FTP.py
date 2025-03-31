87ec7cf2fedf85cccc29c41bcbcbd423ecd01206
KyleWilson8263 <145303597+KyleWilson8263@users.noreply.github.com>
2025-03-31 12:53:10 -0500

    updated for 7-bit

--- Programs/Program 3/FTP.py
+++ Programs/Program 3/FTP.py
@@ -3,18 +3,22 @@ from ftplib import FTP
 #server login info--------------------------------------------
 username = "anonymous"
 password = ""
-host     = "192.168.1.176" #this is the host/VM IP you want to connect to
-filePath = "/7" #desired file path location
+port     = 21
+host     = "192.168.1.11" #this is the host/VM IP you want to connect to
+filePath = "/7"           #this is the desired file path location
 #--------------------------------------------------------------
+#if code timeout then change
+USE_PASSIVE = True
 
 #method variable for switching 7 and 10 bits
     #just set to True rn until fully implemented
 METHOD = True
 
-
 #login to server
 server = FTP(host)
+server.connect(host,port)
 server.login(username,password)
+server.set_pasv(USE_PASSIVE)
 
 #Go to specified Path
 server.cwd(filePath)
@@ -33,16 +37,26 @@ plaintext = ""
 for i in files:
     #if 7-bit method
     if(METHOD):
-        #grab the last 7 bits
-        permissions = i[3:10]
-        #translate the characters using the mapping
-        binary = permissions.translate(replacements)
-        #convert and add to plaintext
-        decimal = int(binary,2)
-        letter = chr(decimal)
-        plaintext += letter
+
+        #first 3 bit check to skip trash files
+        filecheck = i[:3]
+        bincheck = filecheck.translate(replacements)
+        decimalcheck = int(bincheck,2)
+
+        if (decimalcheck > 0):
+            plaintext += ""
+
+        else:
+            #grab the last 7 bits and decode
+            permissions = i[3:10]
+            #translate the characters using the mapping
+            binary = permissions.translate(replacements)
+            #convert and add to plaintext
+            decimal = int(binary,2)
+            letter = chr(decimal)
+            plaintext += letter
     
-    #implement 10 bit here
+    #(TO DO) implement 10 bit here
     #else:
     
 print(plaintext)
