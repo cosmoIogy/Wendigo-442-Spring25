d1ed7160cb6c59bdb57378c51f4d2117ae084de0
Blair Bourque <cosmomail12@gmail.com>
2025-04-04 12:03:29 -0500

    challenge

--- Programs/Program 1/binary_decoder.py
+++ Programs/Program 1/binary_decoder.py
@@ -1,31 +1,32 @@
-#function to convert binary to decimal to characters
-def convert(num):
-    decimal = int(num,2) #convert binary to decimal
-    letter = chr(decimal) #convert decimal to character
-    return letter
+import sys
 
-#grab input
-binary = input()
-
-#initialize string for plaintext
-plaintext = ""
-
-#check whether input is 7-bit or 8-bit
-if(len(binary) % 7 == 0):
-    Li = 0 ; Ri = 7 #create index markers to mark the first 7 bits
-
-    #loop while the right index marker is not greater than the length  of the input
-    while(Ri <= len(binary)):
-        plaintext += convert(binary[Li:Ri]) #convert the 7 bits into characters and append plaintext to plaintext string
-        Li += 7; Ri += 7 #adjust markers to move onto the next 7 bits
-    print(plaintext)
-else: #8-bit process
-    Li = 0; Ri = 8 #create index markers to mark the first 8 bits
+#function to decode binary string 
+def decode(input,bit):
+    string = ''
     
-    #loop while the right index marker is not greater than the length  of the input
-    while(Ri <= len(binary)):
-        plaintext += convert(binary[Li:Ri]) #convert the 8 bits into characters and append plaintext to plaintext string
-        Li += 8; Ri += 8 #adjust markers to move onto the next 8 bits
-    print(plaintext)
-        
-    
+    for i in range (0, len(input), bit):#start at 0, go to end of string, intervals of bit length
+        string_slice = input[i:i+bit]   #get next slice
+        num = int(string_slice, 2)      #convert to decimal
+
+        #if backspace ID:8 then remove end of string
+        if (num==8):
+            string = string[:-1]
+        else:
+            char = chr(num)          #convert to char
+            string += char
+    return string
+
+#main function------------------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    # Read input and clean
+    input = sys.stdin.read().strip()
+    input = input.replace("\n", "").replace("\r", "").strip()
+
+    #mod % stripped input to check for 7 or 8 bit 
+    if (len(input)%8==0):
+        message = decode(input, 8)
+    else:
+        message = decode(input, 7)
+
+    print ("Decoded Text: " + message)
